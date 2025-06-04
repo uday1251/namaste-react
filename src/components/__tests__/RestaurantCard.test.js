de4fb71a6be311aae1d5a50c95ea5625573316b5
Uday <39271523+uday1251@users.noreply.github.com>
2025-06-04 20:42:43 +0530

    Update RestaurantCard.test.js

--- src/components/__tests__/RestaurantCard.test.js
+++ src/components/__tests__/RestaurantCard.test.js
@@ -2,6 +2,7 @@ import { render, screen } from "@testing-library/react";
 import RestaurantCard from "../RestaurantCard";
 import MOCK_DATA from "../mocks/resCardMock.json";
 import "@testing-library/jest-dom";
+import { withPromtedLabel } from "../RestaurantCard";
 
 it("should render RestaurantCard component with props Data", () => {
   render(<RestaurantCard resData={MOCK_DATA} />);
@@ -13,4 +14,7 @@ it("should render RestaurantCard component with props Data", () => {
 
 it("should render RestaurantCard component with Promoted Label", () => {
   // Home Work - test HOC : withPromtedLabel()
+   render(<withPromtedLabel RestaurantCard={RestaurantCard} />); 
+  const name = screen.getByText("Leon's - Burgers & Wings (Leon Grill)");
+  expect(name).toBeInTheDocument();
 });
